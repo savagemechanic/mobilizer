536052e53cbaeb29c6555abca5e3e199e48fe67e
Savage <seighaf@yahoo.com>
2025-12-31 13:30:19 +0100

    fix  feed filter input

--- apps/backend/src/modules/posts/dto/feed-filter.input.ts
+++ apps/backend/src/modules/posts/dto/feed-filter.input.ts
@@ -1,19 +1,30 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsOptional, IsString } from 'class-validator';
 
 @InputType('FeedFilterInput')
 export class FeedFilterInput {
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   orgId?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   stateId?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   lgaId?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   wardId?: string;
 
   @Field({ nullable: true })
+  @IsOptional()
+  @IsString()
   pollingUnitId?: string;
 }
